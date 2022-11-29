713b64285a23590f765673e55d4981eeec85e805
sonhv3112 <sonhovan3112@gmail.com>
2022-11-29 18:14:14 +0700

    Helu

--- app/src/main/java/com/example/e_commerce/Activity/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/Activity/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, EditProfileActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 }
