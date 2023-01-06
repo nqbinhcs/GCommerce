c0b9527364dc1f699e5f98044bc5baf69a4be8e5
nqbinhcs <quangbinh047@gmail.com>
2023-01-06 15:45:53 +0700

    Add back event in viewing detail recipe

--- app/src/main/java/com/example/e_commerce/Adapter/MessageAdapter.java
+++ app/src/main/java/com/example/e_commerce/Adapter/MessageAdapter.java
@@ -1,7 +1,7 @@
 package com.example.e_commerce.Adapter;
 
 import android.graphics.Color;
-import android.support.annotation.NonNull;
+import android.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
