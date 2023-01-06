77f9481046bb69883532a476821a6a4cb6251d13
cubist38 <74866985+cubist38@users.noreply.github.com>
2023-01-07 00:25:39 +0700

    Merged

--- app/src/main/java/com/example/e_commerce/Adapter/MessageAdapter.java
+++ app/src/main/java/com/example/e_commerce/Adapter/MessageAdapter.java
@@ -2,14 +2,13 @@ package com.example.e_commerce.Adapter;
 
 import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.e_commerce.Model.Messages;
